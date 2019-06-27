9e1bd2bb5461ab538278b6f8dbb8c3f31c20f7ad
Nam Nguyen <49970766+ivcnamnguyen@users.noreply.github.com>
2019-06-27 09:36:12 +0700

    source

---
+++ KnobControl/KnobControl/KnobControl.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl.h
+//  KnobControl
+//
+//  Created by Nam Nguyen on 26/6/2019.
+//  Copyright © 2019 Sou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KnobControl.
+FOUNDATION_EXPORT double KnobControlVersionNumber;
+
+//! Project version string for KnobControl.
+FOUNDATION_EXPORT const unsigned char KnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl/PublicHeader.h>
+
+
